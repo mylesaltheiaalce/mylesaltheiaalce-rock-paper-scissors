7a8dacfb766ebd53d35b9b16e43ab7ed0dec4f67
Myles Altheia Alce <mylesalce6@gmail.com>
2026-04-05 17:29:40 +0800

    add computer score display variable

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const playerDisplay = document.getElementById('player-choice');
 const computerDisplay = document.getElementById('computer-choice');
 const resultDisplay = document.getElementById('game-result');
 const playerScoreDisplay = document.getElementById('player-score');
+const computerScoreDisplay = document.getElementById('computer-score');
