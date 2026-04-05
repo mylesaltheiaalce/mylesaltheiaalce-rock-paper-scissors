9bb47e5b4850901ed5570cc2b6f9b05332efd282
Myles Altheia Alce <mylesalce6@gmail.com>
2026-04-05 17:24:02 +0800

    add computer choice display variable

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 const buttons = document.querySelectorAll('.choice-btn');
 const playerDisplay = document.getElementById('player-choice');
+const computerDisplay = document.getElementById('computer-choice');
