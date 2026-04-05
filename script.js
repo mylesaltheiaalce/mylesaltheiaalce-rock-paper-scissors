cb905d05e06c991a403c52b67a2c34f2b27fb456
Myles Altheia Alce <mylesalce6@gmail.com>
2026-04-05 17:31:01 +0800

    initialize score variables for player and computer

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const computerDisplay = document.getElementById('computer-choice');
 const resultDisplay = document.getElementById('game-result');
 const playerScoreDisplay = document.getElementById('player-score');
 const computerScoreDisplay = document.getElementById('computer-score');
+
+let playerScore = 0;
+let computerScore = 0;
