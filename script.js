d25a04c9263321964402a2524a2a3f5c5f189dba
Myles Altheia Alce <mylesalce6@gmail.com>
2026-04-05 17:27:51 +0800

    add game result display variable

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const buttons = document.querySelectorAll('.choice-btn');
 const playerDisplay = document.getElementById('player-choice');
 const computerDisplay = document.getElementById('computer-choice');
+const resultDisplay = document.getElementById('game-result');
